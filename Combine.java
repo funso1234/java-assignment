3f6ba753eafd0bce2149dc344411b26f016c7251
funso1234 <samuel08063251499@gmail.com>
2024-04-02 21:20:19 +0100

    adding file to git

---
+++ Combine.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class Combine {
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+	
+		String nigeria = "Nigeria";
+	
+		String africa = "Africa";
+
+		String combination = nigeria  + "" +  "and"  + "" +  africa;
+			
+		System.out.println(combination);
+
+	}
+}
